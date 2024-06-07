e36c30dc029000d5942d9b58ee59c4511d936c85
Elizzer <ravichandranadithya11@gmail.com>
2024-06-07 13:41:55 +0530

    added loading

--- app/dashboard/merchant/loading.tsx
+++ app/dashboard/merchant/loading.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function loading() {
   return (
-    <div className='w-full h-full flex justify-center items-center font-bold text-3xl'>loading...</div>
-  )
+    <div className="w-full h-full flex justify-center items-center font-bold text-3xl">
+      <div className="loading">
+        <svg width="64px" height="48px">
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="back"
+          ></polyline>
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="front"
+          ></polyline>
+        </svg>
+      </div>
+    </div>
+  );
 }
